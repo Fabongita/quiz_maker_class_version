80aa38eecbe81e8d8aec72667832acd7abc74021
Fabongita <nashmpiii@gmail.com>
2025-05-20 00:12:35 +0800

    added libraries from the previous quiz maker program

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,4 +1,11 @@
+# Import tkinker for the user interface  
+from tkinter import *
+from tkinter import ttk, simpledialog  #added simple dialogue for simple user interaction
+# Import random to make sure that the correct answer is at least randomly in a b c or d
+import random
+import json 
+
 # add class for the whole quiz creator
 class QuizCreator:
     def __init__(self):
-        ...
+        
