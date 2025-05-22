779a5e7371daae9e726033e9339e87ac240838c4
Fabongita <nashmpiii@gmail.com>
2025-05-22 13:00:12 +0800

    added method for adding questions to json file

--- saved_quizzes_manager.py
+++ saved_quizzes_manager.py
@@ -1,3 +1,8 @@
+from quiz_question import QuizQuestion
+
 class SavedQuizzesManager:
     def __init__(self, file_name = "questions_and_answers.json",):
         self.file_name = file_name
+        
+    def add_questions_to_json(self):
+        pass
