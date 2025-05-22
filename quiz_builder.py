b23404e606b736a84ecf774a8c039f60fd0c1cc5
Fabongita <nashmpiii@gmail.com>
2025-05-22 12:02:42 +0800

    added a method for adding a question. The method parameters arefor correct and wrong answers, as well as for the question.

--- quiz_builder.py
+++ quiz_builder.py
@@ -1,7 +1,12 @@
 from quiz_question import QuizQuestion
+import random
 
 class QuizBuilder():
     def __init__(self, quiz_name):
         self.quiz_name = quiz_name
         self.questions = []
-        
+    def add_question(self, correct_answer, wrong_answers, question):
+        option_label = ["a", "b", "c", "d"]
+        all_answers = [correct_answer] + wrong_answers
+        random.shuffle(all_answers)
+
