6bdd7d9a8f19721d1212869009dc727b82f7b0a7
Fabongita <nashmpiii@gmail.com>
2025-05-22 10:32:38 +0800

    added constructor for the QuizBuilder class

--- quiz_builder.py
+++ quiz_builder.py
@@ -1,2 +1,6 @@
 class QuizBuilder:
-    ...
+    def __init__(self, quiz_name, question, correct_answer, options):
+        self.quiz_name = quiz_name
+        self.question = question
+        self.correct_answer = correct_answer
+        self.options = options
