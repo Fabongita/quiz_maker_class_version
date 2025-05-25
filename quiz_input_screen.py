13dfae46f8daa117dc23a946b332078bf4aced96
Fabongita <nashmpiii@gmail.com>
2025-05-26 00:58:45 +0800

    added instance variable so that I can call the quiz builder class which I can use to add the values from quiz name, correct answer, and question

--- quiz_input_screen.py
+++ quiz_input_screen.py
@@ -4,14 +4,14 @@ from tkinter import simpledialog
 class QuizInputScreen:
     def __init__(self, png_pic = "quiz_maker_pic.png"):
        self.picture = png_pic
-    
+       self.quiz_builder = None
     def ask_strings(self):
        quiz_name = simpledialog.askstring("input name of the quiz", "Input the name of the quiz") #ask user to add quiz name
        question = simpledialog.askstring("input question", "Think of a multiple choice question and input it here (enter nothing if you are done): ") # asks users to add there question, and add blank if they want to stop
        if not question:
-           break
-       
-       correct_answer = simpledialog.askstring("Input correct answer", "input the correct answer: ") #ask user to input the correct answer
+          break
+       self.quiz_builder = QuizBuilder(quiz_name)
+       correct_answer = simpledialog.askstring("Input correct answer", "input the correct answer: ") #ask user to input the correct answer    
        wrong_answers = []
         #create a for loop that repeats the askstring function 3 times
        for i in range(3):
@@ -23,4 +23,5 @@ class QuizInputScreen:
        if len(wrong_answers) != 3: # checks if the wrong answers are not exactly 3
            print("Please input exactly 3 answers")
            return 
+       self.quiz_builder.add_question(correct_answer, wrong_answers, question)
        
