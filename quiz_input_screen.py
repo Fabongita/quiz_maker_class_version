4844cdcd06f2b8862cfa0d8c29d54d73226af633
Fabongita <nashmpiii@gmail.com>
2025-05-25 23:58:06 +0800

    added library, imported quiz builder, and added method

--- quiz_input_screen.py
+++ quiz_input_screen.py
@@ -1,3 +1,8 @@
+from quiz_builder import QuizBuilder
+from tkinter import simpledialog
+
 class QuizInputScreen:
-    def __init__(self):
-        ...
+    def __init__(self, png_pic = "quiz_maker_pic.png"):
+        picture = png_pic
+    
+    def ask_strings(self):
