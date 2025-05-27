71067c7ac13cb5f993cb7eabc86410e3a353c74f
Fabongita <nashmpiii@gmail.com>
2025-05-28 02:11:16 +0800

    initialized GUI, created instance, called widgets(maybe still incomplete), ran the main loop

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from tkinter import *
+from quiz_input_screen import QuizInputScreen
 #Initialize the GUI.
+root = Tk()
 #Create an instance of QuizInputScreen.
+quiz_input_screen = QuizInputScreen()
 #Call its widgets() method to load up the interface.
+quiz_input_screen.widgets
 #Run the tkinter main loop.
+root.mainloop
