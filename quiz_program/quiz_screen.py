96662c7ad7c6ff6a50668ad51ab1cc2f99732c43
Fabongita <nashmpiii@gmail.com>
2025-05-28 13:37:41 +0800

    copy pasted the code for the buttons from the original quiz program. And I also tweaked the variables by adding 'self.' so that it instantiates. And I also renamed the other methods

--- quiz_program/quiz_screen.py
+++ quiz_program/quiz_screen.py
@@ -7,30 +7,38 @@ class WidgetsToBeUsed:
         self.start_frame = Frame(self.root)
         self.saved_quizzes_frame = Frame(self.root)
         self.play_frame = Frame(self.root)
+        self.button_frame = Frame(self.intro_frame)
+        self.button_frame.pack(side="bottom", padx="10")
 
     # method for saved quizzes button
-    def saved_quizzes_button(self):
-        ...               
+    def saved_quiz_button(self):
+        self.saved_quizzes_button = Button(self.button_frame, text="Saved Quizzes", height="7", width="20",  activebackground="blue", activeforeground="yellow", command=saved_quizzes )
+        self.saved_quizzes_button.pack(padx="10", pady="70", side="left")                
 
     # method for start button
-    def start_button(self):
-        ...
+    def starter_button(self):
+        self.start_button = Button(self.button_frame, text="Start game", height="7", width="20",  activebackground="blue", activeforeground="yellow", command=start_button_logic )
+        self.start_button.pack(padx="10", pady="70", side="left") 
 
     # method for main menu button
-    def main_menu_button(self):
-        ...
+    def menu_button(self):
+        self.main_menu_button = Button(self.play_frame, text="Main menu", command=lambda: self.intro_frame.tkraise())
+        self.main_menu_button.pack(padx="20", pady="20")
 
     # method for create quiz button
     def create_quiz_button(self):
-        ...
+        self.quiz_creator_button = Button(self.button_frame, text="Create Quiz", height="7", width="20",  activebackground="blue", activeforeground="yellow", command=quiz_maker )
+        self.quiz_creator_button.pack(padx="10", pady="70", side="left") 
 
     # method for submit button
-    def submit_button(self):
-        ...
+    def submitter_button(self):
+        self.submit_button = Button(self.play_frame, text="Submit", command=submit)
+        self.submit_button.pack(pady=10)
 
     # method for back button
     def back_button(self):
-        ...
+        self.Back_button_saved_quizzes = Button(self.saved_quizzes_frame, text="Back", command=lambda: intro_frame.tkraise())
+        self.Back_button_saved_quizzes.pack(padx="20", pady="20", side="right")
 
     # method for radio buttons
     def radio_buttons(self):
@@ -39,7 +47,7 @@ class WidgetsToBeUsed:
     # method for listbox
     def listbox(self):
         ...
-        
+
     #method for scrollbar
     def scrollbar(self):
         ...
