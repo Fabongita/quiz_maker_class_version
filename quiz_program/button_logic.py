d0f5dadae4e232e6d118caae9ed3d1bf397c2171
Fabongita <nashmpiii@gmail.com>
2025-05-28 19:12:41 +0800

    removed global

--- quiz_program/button_logic.py
+++ quiz_program/button_logic.py
@@ -5,14 +5,13 @@ class ButtonLogic:
         self.current_index = current_index
         self.score = score
     def start(self, questions):
-        global self.current_questions, self.current_index, self.score
         self.current_questions = questions
         self.current_index = 0
         self.score = 0
         
         show_question() 
         
-        main_menu_button.pack_forget()    
+        self.main_menu_button.pack_forget()    
         
         for widget in radiobutton_widgets.values():
             widget.pack(fill="x", pady="2")
