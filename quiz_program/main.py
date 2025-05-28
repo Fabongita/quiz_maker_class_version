c98d0c6173d1aa87695c8d8b77873cd1bec2a987
Fabongita <nashmpiii@gmail.com>
2025-05-29 01:18:15 +0800

    imported the class from both button logic and quiz screen

--- quiz_program/main.py
+++ quiz_program/main.py
@@ -1,5 +1,7 @@
 import sys
 import os
 from tkinter import Tk
+from quiz_screen import QuizScreen
+from button_logic import ButtonLogic
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
