39919444388f1f117639ee065b68af38ae1d0242
Fabongita <nashmpiii@gmail.com>
2025-05-28 19:05:09 +0800

    copy pasted the start function from the quiz program

--- quiz_program/button_logic.py
+++ quiz_program/button_logic.py
@@ -3,4 +3,18 @@ class ButtonLogic:
         self.current_questions = current_questions
         self.current_index = current_index
         self.score = score
+    def start(questions):
+        global current_questions, current_index, score
+        current_questions = questions
+        current_index = 0
+        score = 0
+        
+        show_question() 
+        
+        main_menu_button.pack_forget()    
+        
+        for widget in radiobutton_widgets.values():
+            widget.pack(fill="x", pady="2")
+        submit_button.pack(pady=10)
+        play_frame.tkraise()
 
