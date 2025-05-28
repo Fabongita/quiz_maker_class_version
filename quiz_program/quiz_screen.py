a6f4bc8fa3bf068e422ec8ba6599264044072818
Fabongita <nashmpiii@gmail.com>
2025-05-28 12:33:23 +0800

    added methods

--- quiz_program/quiz_screen.py
+++ quiz_program/quiz_screen.py
@@ -1,5 +1,15 @@
 class WidgetsToBeUsed:
+
     # constructor method
+    def __init__(self):
+        ...
     # method for frame
+    def frame(self):
+        ...
+
     # method for buttons
+    def buttons(self):
+        ...
     # method for radio buttons
+    def radio_buttons(self):
+        ...
