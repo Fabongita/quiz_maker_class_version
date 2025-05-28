830cba4d51197a0826c8a625febff0a83d7303f8
Fabongita <nashmpiii@gmail.com>
2025-05-29 01:21:47 +0800

    added function and pseudo-code

--- quiz_program/main.py
+++ quiz_program/main.py
@@ -5,3 +5,13 @@ from quiz_screen import QuizScreen
 from button_logic import ButtonLogic
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+#add main function that sets up and runs the quiz GUI app 
+def main():
+    # create main window
+    # initialize UI handler
+    # build GUI widgets
+    # connect UI with logic
+    # configure button commands
+    # raise intro frame
+    # start GUI event loop
