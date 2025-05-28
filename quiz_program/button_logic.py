e386a7db20683ef51e0870f36e9a368b0baef799
Fabongita <nashmpiii@gmail.com>
2025-05-28 19:00:50 +0800

    added constructor variable and added constructor parameters

---
+++ quiz_program/button_logic.py
@@ -0,0 +1,6 @@
+class ButtonLogic:
+    def __init__(self, current_questions = [], current_index = 0, score = 0):
+        self.current_questions = current_questions
+        self.current_index = current_index
+        self.score = score
+
